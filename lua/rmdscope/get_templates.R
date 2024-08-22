ebf03edf7e592ee56130eeb2b9e8c3076574f550
James Conigrave <james.conigrave@gmail.com>
2024-08-22 16:08:07 +1000

    suppress startup messages in R

--- lua/rmdscope/get_templates.R
+++ lua/rmdscope/get_templates.R
@@ -1,27 +1,32 @@
-# Taken from vscode-R extension
+get_templates_json <- function() {
+  # Suppress package startup messages
+  suppressMessages(suppressWarnings(require(jsonlite, quietly = TRUE)))
+  suppressMessages(suppressWarnings(require(yaml, quietly = TRUE)))
 
-loadNamespace("jsonlite")
-loadNamespace("yaml")
-
-pkgs <- .packages(all.available = TRUE)
-templates <- new.env()
-template_dirs <- lapply(pkgs, function(pkg) {
+  # Your existing code to gather templates
+  pkgs <- .packages(all.available = TRUE)
+  templates <- new.env()
+  template_dirs <- lapply(pkgs, function(pkg) {
     dir <- system.file("rmarkdown/templates", package = pkg)
     if (dir.exists(dir)) {
-        ids <- list.dirs(dir, full.names = FALSE, recursive = FALSE)
-        for (id in ids) {
-            file <- file.path(dir, id, "template.yaml")
-            if (file.exists(file)) {
-                data <- yaml::read_yaml(file)
-                data$id <- id
-                data$package <- pkg
-                templates[[paste0(pkg, "::", id)]] <- data
-            }
+      ids <- list.dirs(dir, full.names = FALSE, recursive = FALSE)
+      for (id in ids) {
+        file <- file.path(dir, id, "template.yaml")
+        if (file.exists(file)) {
+          data <- yaml::read_yaml(file)
+          data$id <- id
+          data$package <- pkg
+          templates[[paste0(pkg, "::", id)]] <- data
         }
+      }
     }
-})
+  })
 
-template_list <- unname(as.list(templates))
-lim <- Sys.getenv("VSCR_LIM")
-json <- jsonlite::toJSON(template_list, auto_unbox = TRUE)
-cat(lim, json, lim, sep = "\n", file = stdout())
+  template_list <- unname(as.list(templates))
+  json <- jsonlite::toJSON(template_list, auto_unbox = TRUE)
+
+  # Return the JSON string
+  cat(json)
+}
+
+get_templates_json()
