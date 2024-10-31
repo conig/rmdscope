5b1e67de2df65e852cc5c9a8b57dc65f625e7fe6
James Conigrave <james.conigrave@gmail.com>
2024-11-01 09:47:54 +1100

    abstract get_word

--- lua/rmdscope/telescope.lua
+++ lua/rmdscope/telescope.lua
@@ -5,183 +5,205 @@ local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local previewers = require("telescope.previewers")
 local utils = require("rmdscope.utils")
-local lib = require("nvim-tree.lib")  -- Use nvim-tree's lib module
+local lib = require("nvim-tree.lib") -- Use nvim-tree's lib module
 
 local M = {}
 
 function M.templates()
-  local templates = utils.get_templates()
+	local templates = utils.get_templates()
 
-  pickers.new({}, {
-    prompt_title = "RMD Templates",
-    finder = finders.new_table {
-      results = templates,
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = entry.name .. " (" .. entry.package .. ")",
-          ordinal = entry.name,
-          path = entry.path,
-        }
-      end,
-    },
-    sorter = conf.generic_sorter({}),
-    previewer = previewers.new_buffer_previewer({
-      title = "Template Preview",
-      define_preview = function(self, entry)
-        local template_content = utils.read_template(entry.path)
-        if template_content then
-          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, vim.split(template_content, "\n"))
-        else
-          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, {"Error reading template"})
-        end
-      end,
-    }),
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        local selection = action_state.get_selected_entry()
-        actions.close(prompt_bufnr)
+	pickers
+		.new({}, {
+			prompt_title = "RMD Templates",
+			finder = finders.new_table({
+				results = templates,
+				entry_maker = function(entry)
+					return {
+						value = entry,
+						display = entry.name .. " (" .. entry.package .. ")",
+						ordinal = entry.name,
+						path = entry.path,
+					}
+				end,
+			}),
+			sorter = conf.generic_sorter({}),
+			previewer = previewers.new_buffer_previewer({
+				title = "Template Preview",
+				define_preview = function(self, entry)
+					local template_content = utils.read_template(entry.path)
+					if template_content then
+						vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, vim.split(template_content, "\n"))
+					else
+						vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, { "Error reading template" })
+					end
+				end,
+			}),
+			attach_mappings = function(prompt_bufnr, map)
+				actions.select_default:replace(function()
+					local selection = action_state.get_selected_entry()
+					actions.close(prompt_bufnr)
 
-        -- Get the currently selected node in nvim-tree
-        local node = lib.get_node_at_cursor()
+					-- Get the currently selected node in nvim-tree
+					local node = lib.get_node_at_cursor()
 
-        local dir_path
+					local dir_path
 
-        if node then
-          -- Check if the node is a directory
-          if node.fs_stat and node.fs_stat.type == "directory" then
-            dir_path = node.absolute_path
-          else
-            -- Use the parent directory of the file node
-            dir_path = node.parent.absolute_path
-          end
-        else
-          -- If no node is selected, use the current working directory
-          dir_path = vim.fn.getcwd()
-        end
+					if node then
+						-- Check if the node is a directory
+						if node.fs_stat and node.fs_stat.type == "directory" then
+							dir_path = node.absolute_path
+						else
+							-- Use the parent directory of the file node
+							dir_path = node.parent.absolute_path
+						end
+					else
+						-- If no node is selected, use the current working directory
+						dir_path = vim.fn.getcwd()
+					end
 
-        -- Provide the directory path as default value
-        local default_value = dir_path .. "/"
+					-- Provide the directory path as default value
+					local default_value = dir_path .. "/"
 
-        -- Prompt the user for the filename using vim.fn.input
-        local filename = vim.fn.input("Save as: ", default_value)
-        if filename and filename ~= "" then
-          utils.save_template(selection.path, filename)
-          print("Template saved to: " .. filename)
-        else
-          print("No filename provided, operation cancelled")
-        end
-      end)
-      return true
-    end,
-  }):find()
+					-- Prompt the user for the filename using vim.fn.input
+					local filename = vim.fn.input("Save as: ", default_value)
+					if filename and filename ~= "" then
+						utils.save_template(selection.path, filename)
+						print("Template saved to: " .. filename)
+					else
+						print("No filename provided, operation cancelled")
+					end
+				end)
+				return true
+			end,
+		})
+		:find()
 end
 
 function M.insert_object_member()
+	-- If this file exists, delete it:
+	local temp_path = "/tmp/nvim-rmdclip/menu.json"
+	if vim.fn.filereadable(temp_path) == 1 then
+		vim.fn.delete(temp_path)
+	end
+	-- if there is an error path, delete it
+	local error_path = "/tmp/nvim-rmdclip/error.json"
+	if vim.fn.filereadable(error_path) == 1 then
+		vim.fn.delete(error_path)
+	end
 
-  -- If this file exists, delete it:
-  local temp_path = "/tmp/rmdclip/menu.json"
-  if vim.fn.filereadable(temp_path) == 1 then
-    vim.fn.delete(temp_path)
-  end
-  -- Write the object names to the temp file
-  utils.write_object_names()
+	-- Write the object names to the temp file
+	utils.write_object_names()
 
-  -- Wait until the file is writtern
-  local total_wait = 0
-  while vim.fn.filereadable(temp_path) == 0 and total_wait <= 2500 do
-    vim.wait(100)
-    total_wait = total_wait + 100
-  end
+	-- Wait until the file is writtern
+	local total_wait = 0
+	while vim.fn.filereadable(temp_path) == 0 and vim.fn.filereadable(error_path) == 0 and total_wait <= 2000 do
+		vim.wait(20)
+		total_wait = total_wait + 20
+	end
 
-  if total_wait >= 2500 then
-    print("Timeout waiting for temporary file to be written.")
-    return
-  end
+	if total_wait >= 2000 then
+		print("Timeout waiting for temporary file to be written.")
+		return
+	end
 
-  -- Ensure object names are read and placed in the clipboard
-  -- Get the JSON data (list of objects with 'name' and 'contents')
-  local objects = utils.read_object_names()
-  if not objects or vim.tbl_isempty(objects) then
-    print("No objects found.")
-    return
-  end
+	if vim.fn.filereadable(error_path) == 1 then
+		print("Error reading object names.")
+		return
+	end
 
-  -- Set up the Telescope picker
-  pickers.new({}, {
-    prompt_title = "Select Object",
-    finder = finders.new_table {
-      results = objects,
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = entry.name,
-          ordinal = entry.name,
-          contents = entry.contents,
-        }
-      end,
-    },
-    sorter = conf.generic_sorter({}),
-    previewer = previewers.new_buffer_previewer({
-      title = "Object Details",
-      define_preview = function(self, entry)
-        local contents = entry.value.contents
-        if contents then
-          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, vim.split(contents, "\n"))
-        else
-          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, {"No contents available"})
-        end
-      end,
-    }),
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        local selection = action_state.get_selected_entry()
-        actions.close(prompt_bufnr)
+	-- Ensure object names are read and placed in the clipboard
+	-- Get the JSON data (list of objects with 'name' and 'contents')
+	local objects = utils.read_object_names()
+	if not objects or vim.tbl_isempty(objects) then
+		print("No objects found.")
+		return
+	end
 
-        if not selection then
-          print("No selection made.")
-          return
-        end
+	-- Set up the Telescope picker
+	pickers
+		.new({}, {
+			prompt_title = "Select Object",
+			finder = finders.new_table({
+				results = objects,
+				entry_maker = function(entry)
+					return {
+						value = entry,
+						display = entry.name,
+						ordinal = entry.name,
+						contents = entry.contents,
+					}
+				end,
+			}),
+			sorter = conf.generic_sorter({}),
+			previewer = previewers.new_buffer_previewer({
+				title = "Object Details",
+				define_preview = function(self, entry)
+					local contents = entry.value.contents
+					if contents then
+						vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, vim.split(contents, "\n"))
+					else
+						vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, { "No contents available" })
+					end
+				end,
+			}),
+			attach_mappings = function(prompt_bufnr, map)
+				actions.select_default:replace(function()
+					local selection = action_state.get_selected_entry()
+					actions.close(prompt_bufnr)
 
-        local selected_name = selection.value.name
-        vim.notify("Selected name: " .. selected_name, vim.log.levels.INFO)
+					if not selection then
+						print("No selection made.")
+						return
+					end
 
-        -- Get cursor position and line
-        local cursor_pos = vim.api.nvim_win_get_cursor(0)
-        local row = cursor_pos[1]
-        local col = cursor_pos[2]
-        local line = vim.api.nvim_get_current_line()
-        vim.notify(string.format("Cursor position before: row=%d, col=%d", row, col), vim.log.levels.DEBUG)
-        vim.notify("Current line: " .. line, vim.log.levels.DEBUG)
+					local selected_name = selection.value.name
+					vim.notify("Selected name: " .. selected_name, vim.log.levels.INFO)
 
-        -- Find the end of the word under the cursor
-        -- Adjust the pattern if your object names include characters other than alphanumerics
-        local word_start, word_end_col = line:find("([_%w]+)", col + 1)
+					-- Get cursor position and line
+					local cursor_pos = vim.api.nvim_win_get_cursor(0)
+					local row = cursor_pos[1]
+					local col = cursor_pos[2]
+					local line = vim.api.nvim_get_current_line()
+					vim.notify(string.format("Cursor position before: row=%d, col=%d", row, col), vim.log.levels.DEBUG)
+					vim.notify("Current line: " .. line, vim.log.levels.DEBUG)
 
-        if word_end_col then
-          -- Move cursor to the end of the word
-          vim.api.nvim_win_set_cursor(0, { row, word_end_col })
-          vim.notify(string.format("Cursor moved to end of word at col=%d", word_end_col), vim.log.levels.DEBUG)
-        else
-          -- If no word is found after the cursor, move to the end of the line
-          local line_length = #line
-          vim.api.nvim_win_set_cursor(0, { row, line_length })
-          vim.notify(string.format("No word found after cursor. Moved cursor to end of line at col=%d", line_length), vim.log.levels.DEBUG)
-        end
+					-- Find the end of the word under the cursor
+					-- Adjust the pattern if your object names include characters other than alphanumerics
+					local word_start, word_end_col = line:find("([_%w]+)", col + 1)
 
-        -- Insert the dollar symbol and the selected name
-        -- Using vim.api.nvim_put
-        vim.api.nvim_put({ '$' .. selected_name }, 'c', true, true)
-        vim.notify("Inserted $" .. selected_name, vim.log.levels.INFO)
-      end)
-      return true
-    end,
-  }):find()
+					if word_end_col then
+						-- Move cursor to the end of the word
+						vim.api.nvim_win_set_cursor(0, { row, word_end_col })
+						vim.notify(
+							string.format("Cursor moved to end of word at col=%d", word_end_col),
+							vim.log.levels.DEBUG
+						)
+					else
+						-- If no word is found after the cursor, move to the end of the line
+						local line_length = #line
+						vim.api.nvim_win_set_cursor(0, { row, line_length })
+						vim.notify(
+							string.format(
+								"No word found after cursor. Moved cursor to end of line at col=%d",
+								line_length
+							),
+							vim.log.levels.DEBUG
+						)
+					end
+
+					-- Insert the dollar symbol and the selected name
+					-- Using vim.api.nvim_put
+					vim.api.nvim_put({ "$" .. selected_name }, "c", true, true)
+					vim.notify("Inserted $" .. selected_name, vim.log.levels.INFO)
+				end)
+				return true
+			end,
+		})
+		:find()
 end
 
-
 function M.load_extension()
-  require("telescope").load_extension("rmdscope")
+	require("telescope").load_extension("rmdscope")
 end
 
 return M
--- lua/rmdscope/utils.lua
+++ lua/rmdscope/utils.lua
@@ -2,173 +2,231 @@ local M = {}
 
 -- Function to read the contents of a file
 local function read_file(filepath)
-  local file = io.open(filepath, "r")
-  if not file then
-    error("Could not read file: " .. filepath)
-  end
-  local content = file:read("*a")
-  file:close()
-  return content
+	local file = io.open(filepath, "r")
+	if not file then
+		error("Could not read file: " .. filepath)
+	end
+	local content = file:read("*a")
+	file:close()
+	return content
 end
 
 -- Function to get the list of R package templates
 function M.get_templates()
-  -- Set the correct path to the R script
-  local script_path = vim.fn.stdpath("data") .. "/lazy/rmdscope/lua/rmdscope/get_templates.R"
+	-- Set the correct path to the R script
+	local script_path = vim.fn.stdpath("data") .. "/lazy/rmdscope/lua/rmdscope/get_templates.R"
 
-  -- Use the R script to get the list of available templates in JSON format
-  local templates_json_str = vim.fn.system("Rscript " .. script_path)
-  -- Parse the JSON output from the R script using Neovim's built-in JSON decoder
-  local templates = vim.fn.json_decode(templates_json_str)
-  if not templates then
-    error("Error parsing JSON.")
-  end
+	-- Use the R script to get the list of available templates in JSON format
+	local templates_json_str = vim.fn.system("Rscript " .. script_path)
+	-- Parse the JSON output from the R script using Neovim's built-in JSON decoder
+	local templates = vim.fn.json_decode(templates_json_str)
+	if not templates then
+		error("Error parsing JSON.")
+	end
 
-  -- Return the parsed JSON as a Lua table
-  return templates
+	-- Return the parsed JSON as a Lua table
+	return templates
 end
 
 -- Function to read a template file
 function M.read_template(template_path)
-  return read_file(template_path)
+	return read_file(template_path)
 end
 
 -- Updated function to save a template to a file, supporting subdirectories
 function M.save_template(template_path, filename)
-  local content = M.read_template(template_path)
-  
-  -- Extract directory path from filename
-  local dir_path = vim.fn.fnamemodify(filename, ':h')
-  
-  -- Create directory if it doesn't exist
-  if dir_path ~= '.' and vim.fn.isdirectory(dir_path) == 0 then
-    local create_dir = vim.fn.input("Directory doesn't exist. Create it? (y/n): ")
-    if create_dir:lower() == 'y' then
-      vim.fn.mkdir(dir_path, "p")
-    else
-      print("Operation cancelled")
-      return
-    end
-  end
+	local content = M.read_template(template_path)
 
-  if vim.fn.filereadable(filename) == 1 then
-    local answer = vim.fn.input("File exists. Overwrite? (y/n): ")
-    if answer:lower() ~= 'y' then
-      print("Operation cancelled")
-      return
-    end
-  end
+	-- Extract directory path from filename
+	local dir_path = vim.fn.fnamemodify(filename, ":h")
 
-  local file = io.open(filename, "w")
-  if not file then
-    error("Could not write to file: " .. filename)
-  end
-  file:write(content)
-  file:close()
-  
-  print("Template saved to " .. filename)
+	-- Create directory if it doesn't exist
+	if dir_path ~= "." and vim.fn.isdirectory(dir_path) == 0 then
+		local create_dir = vim.fn.input("Directory doesn't exist. Create it? (y/n): ")
+		if create_dir:lower() == "y" then
+			vim.fn.mkdir(dir_path, "p")
+		else
+			print("Operation cancelled")
+			return
+		end
+	end
+
+	if vim.fn.filereadable(filename) == 1 then
+		local answer = vim.fn.input("File exists. Overwrite? (y/n): ")
+		if answer:lower() ~= "y" then
+			print("Operation cancelled")
+			return
+		end
+	end
+
+	local file = io.open(filename, "w")
+	if not file then
+		error("Could not write to file: " .. filename)
+	end
+	file:write(content)
+	file:close()
+
+	print("Template saved to " .. filename)
 end
 
 -- Updated function to create a floating window for input
 function M.create_input_popup(prompt, callback)
-  local function create_float()
-    local width = 60
-    local height = 1
-    local buf = vim.api.nvim_create_buf(false, true)
-    local win_opts = {
-      relative = 'editor',
-      width = width,
-      height = height,
-      row = math.floor((vim.o.lines - height) / 2),
-      col = math.floor((vim.o.columns - width) / 2),
-      style = 'minimal',
-      border = 'single',
-    }
-    local win = vim.api.nvim_open_win(buf, true, win_opts)
-    return buf, win
-  end
+	local function create_float()
+		local width = 60
+		local height = 1
+		local buf = vim.api.nvim_create_buf(false, true)
+		local win_opts = {
+			relative = "editor",
+			width = width,
+			height = height,
+			row = math.floor((vim.o.lines - height) / 2),
+			col = math.floor((vim.o.columns - width) / 2),
+			style = "minimal",
+			border = "single",
+		}
+		local win = vim.api.nvim_open_win(buf, true, win_opts)
+		return buf, win
+	end
 
-  local buf, win = create_float()
+	local buf, win = create_float()
 
-  -- Set buffer options using vim.bo with buffer ID
-  vim.bo[buf].buftype = 'prompt'
-  vim.bo[buf].bufhidden = 'wipe'
+	-- Set buffer options using vim.bo with buffer ID
+	vim.bo[buf].buftype = "prompt"
+	vim.bo[buf].bufhidden = "wipe"
 
-  -- Set prompt before entering insert mode
-  vim.fn.prompt_setprompt(buf, prompt .. ' ')
-  
-  -- Force a screen redraw to ensure the prompt is visible
-  vim.cmd('redraw')
-  
-  -- Defer entering insert mode to ensure the prompt is rendered
-  vim.defer_fn(function()
-    vim.api.nvim_set_current_buf(buf)
-    vim.cmd('startinsert!')
-  end, 20)
+	-- Set prompt before entering insert mode
+	vim.fn.prompt_setprompt(buf, prompt .. " ")
 
-  -- Set callback for when Enter is pressed
-  vim.keymap.set('i', '<CR>', function()
-    local input = vim.trim(vim.api.nvim_buf_get_lines(buf, 0, -1, false)[1]:sub(#prompt + 2))
-    vim.api.nvim_win_close(win, true)
-    vim.schedule(function()
-      callback(input)
-    end)
-  end, { buffer = buf, noremap = true, silent = true })
+	-- Force a screen redraw to ensure the prompt is visible
+	vim.cmd("redraw")
 
-  -- Set callback for when Esc is pressed
-  vim.keymap.set('i', '<Esc>', function()
-    vim.api.nvim_win_close(win, true)
-    vim.schedule(function()
-      callback(nil)
-    end)
-  end, { buffer = buf, noremap = true, silent = true })
+	-- Defer entering insert mode to ensure the prompt is rendered
+	vim.defer_fn(function()
+		vim.api.nvim_set_current_buf(buf)
+		vim.cmd("startinsert!")
+	end, 20)
+
+	-- Set callback for when Enter is pressed
+	vim.keymap.set("i", "<CR>", function()
+		local input = vim.trim(vim.api.nvim_buf_get_lines(buf, 0, -1, false)[1]:sub(#prompt + 2))
+		vim.api.nvim_win_close(win, true)
+		vim.schedule(function()
+			callback(input)
+		end)
+	end, { buffer = buf, noremap = true, silent = true })
+
+	-- Set callback for when Esc is pressed
+	vim.keymap.set("i", "<Esc>", function()
+		vim.api.nvim_win_close(win, true)
+		vim.schedule(function()
+			callback(nil)
+		end)
+	end, { buffer = buf, noremap = true, silent = true })
+end
+
+local get_word = function()
+  local word = vim.fn.expand("<cword>")
+  return word
 end
 
 -- Function to read object names
 function M.write_object_names()
-  -- Get the word under the cursor
-  local word = vim.fn.expand("<cword>")
+	-- Get the current visual mode ('v' for character-wise, 'V' for line-wise, etc.)
+	local mode = vim.fn.mode()
+	local bufnr = vim.api.nvim_get_current_buf()
 
-  -- Escape double quotes in word
-  word = word:gsub('"', '\\"')
+	local obj
 
-  -- Format the command string with properly escaped quotes
-  local cmd = 'nvimscope.r::nvimclip("' .. word .. '")'
+	if mode == "V" then
+		-- Handle line-wise visual mode
+		local start_line = vim.fn.line("v")
+		local end_line = vim.fn.line(".")
 
-  -- Send the command to slimetree
-  require("nvim-slimetree").goo_send(cmd)
+		-- Ensure start_line is before end_line
+		if start_line > end_line then
+			start_line, end_line = end_line, start_line
+		end
+
+		-- Get the entire lines in the selection
+		local selected_lines = vim.api.nvim_buf_get_lines(
+			bufnr,
+			start_line - 1, -- 0-indexed
+			end_line, -- end_line is exclusive
+			false
+		)
+
+		-- Concatenate lines into a single string
+		obj = table.concat(selected_lines, "\n")
+		obj = obj:gsub("^%s*(.-)%s*$", "%1") -- Trim leading and trailing whitespace
+	elseif mode:sub(1, 1) == "v" then
+		-- Handle character-wise visual mode ('v' or '' for block-wise)
+		local start_line, start_col = vim.fn.line("v"), vim.fn.col("v")
+		local end_line, end_col = vim.fn.line("."), vim.fn.col(".")
+
+		-- Ensure start position is before end position
+		if (start_line > end_line) or (start_line == end_line and start_col > end_col) then
+			start_line, end_line = end_line, start_line
+			start_col, end_col = end_col, start_col
+		end
+
+		-- Retrieve the selected text
+		local selected_text = vim.api.nvim_buf_get_text(
+			bufnr,
+			start_line - 1, -- 0-indexed
+			start_col - 1, -- 0-indexed
+			end_line - 1, -- 0-indexed
+			end_col, -- end_col is exclusive
+			{}
+		)
+
+		-- Concatenate the selected text into a single string
+		obj = table.concat(selected_text, "\n")
+		obj = obj:gsub("^%s*(.-)%s*$", "%1") -- Trim leading and trailing whitespace
+	else
+		-- No visual selection; default to the word under the cursor
+		obj = get_word()
+	end
+
+	-- Escape double quotes in the object to prevent command injection issues
+	obj = obj:gsub('"', '\\"')
+
+	-- Depending on your needs, you might want to wrap obj in quotes
+	-- For example, if obj is a string argument
+	local cmd = "nvimscope.r::nvimclip(" .. obj .. ")"
+
+	-- Send the command to slimetree
+	require("nvim-slimetree").goo_send(cmd)
 end
 
 function M.read_object_names()
- -- Define the path to the temporary JSON file
-  local temp_path = "/tmp/rmdclip/menu.json"
+	-- Define the path to the temporary JSON file
+	local temp_path = "/tmp/nvim-rmdclip/menu.json"
 
-  -- Check if the temporary file exists
-  if vim.fn.filereadable(temp_path) ~= 1 then
-    print("Temporary file does not exist: " .. temp_path)
-    return nil
-  end
+	-- Check if the temporary file exists
+	if vim.fn.filereadable(temp_path) ~= 1 then
+		print("Temporary file does not exist: " .. temp_path)
+		return nil
+	end
 
-  -- Read the contents of the temporary file
-  local json_str = read_file(temp_path)
+	-- Read the contents of the temporary file
+	local json_str = read_file(temp_path)
 
-  -- Decode the JSON string
-  local ok, objects = pcall(vim.fn.json_decode, json_str)
-  if not ok then
-    print('Failed to decode JSON from temp file: ' .. objects)
-    return nil
-  end
+	-- Decode the JSON string
+	local ok, objects = pcall(vim.fn.json_decode, json_str)
+	if not ok then
+		print("Failed to decode JSON from temp file: " .. objects)
+		return nil
+	end
 
-  -- Delete the temporary file after decoding
-  local delete_ok, delete_err = os.remove(temp_path)
-  if not delete_ok then
-    print("Failed to delete temporary file: " .. delete_err)
-    -- Depending on your preference, you might want to return here or continue
-  end
+	-- Delete the temporary file after decoding
+	local delete_ok, delete_err = os.remove(temp_path)
+	if not delete_ok then
+		print("Failed to delete temporary file: " .. delete_err)
+		-- Depending on your preference, you might want to return here or continue
+	end
 
-  -- Return the decoded objects
-  return objects
+	-- Return the decoded objects
+	return objects
 end
 
 return M
-
