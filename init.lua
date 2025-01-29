42fb2fd0cee1a628d1e2e53087f84cec094a1e71
conig <james.conigrave@gmail.com>
2025-01-29 15:21:15 +1100

    move init

--- init.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-function M.setup()
-  require('rmdscope.telescope').load_extension()
-end
-
-return M
-
---
+++ lua/rmdscope/init.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+function M.setup()
+  require('telescope').load_extension()
+end
+
+return M
+
